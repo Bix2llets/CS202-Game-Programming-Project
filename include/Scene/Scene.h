4a5d86c182069b0e2b3e8356c56a53f0d4b40678
Bix2llets <nhtphat2418@apcs.fitus.edu.vn>
2025-05-29 11:20:26 +0700

    Add base class for scenes

---
+++ include/Scene/Scene.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <optional>
+// * Base for the state machine of scenes
+// * Abstract classes, not to instantize
+// * Also singleton
+class Scene : public sf::Drawable {
+   protected:
+    sf::RenderWindow &window;
+
+   public:
+    Scene(sf::RenderWindow &window) : window{window} {};
+
+    virtual void handleEvent(std::optional<sf::Event>& event) = 0;
+    virtual void handleInput(std::optional<sf::Event>& event) = 0;
+    virtual void draw(sf::RenderTarget& target,
+                      sf::RenderStates state) const = 0;
+    virtual void update() = 0;
+    virtual ~Scene() = default;
+};
