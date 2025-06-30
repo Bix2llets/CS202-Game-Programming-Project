214770e691ec1feb70c39e9531882a7d7128a29f
Bix2llets <nhtphat2418@apcs.fitus.edu.vn>
2025-07-01 00:05:34 +0700

    [fix]: Fix bugs for test cases

--- tests/Scene/SceneSwitching.cc
+++ tests/Scene/SceneSwitching.cc
@@ -1,18 +1,23 @@
 #include <gtest/gtest.h>
 
-#include "MainMenuMock.hpp"
 #include "Core/InputManager.hpp"
-#include "Core/SceneManager.hpp"
 #include "Core/ResourceManager.hpp"
-
+#include "Core/SceneManager.hpp"
+#include "MainMenuMock.hpp"
 #include "Scene/BlankScene.hpp"
 TEST(sceneSwitching, actualSwitching) {
     sf::RenderWindow window;
     InputManager inputManager(window);
     SceneManager sceneManager(window);
     ResourceManager resourceManager;
-    MainMenuMock mainMenu(window, "Main Menu", sceneManager, inputManager, resourceManager);
-    sceneManager.registerScene<BlankScene>("Gameplay", inputManager, resourceManager);
+    resourceManager.loadFont(
+        "assets/fonts/League_Spartan/static/LeagueSpartan-Medium.ttf",
+        "LeagueSpartan");
+
+    MainMenuMock mainMenu(window, "Main Menu", sceneManager, inputManager,
+                          resourceManager);
+    sceneManager.registerScene<BlankScene>("Gameplay", inputManager,
+                                           resourceManager);
     mainMenu.simulateTestButtonClick();
     EXPECT_STREQ(sceneManager.getCurrentScene()->getName().c_str(), "Gameplay");
 }
@@ -21,8 +26,14 @@ TEST(sceneSwitching, nullSwitching) {
     InputManager inputManager(window);
     SceneManager sceneManager(window);
     ResourceManager resourceManager;
-    MainMenuMock mainMenu(window, "Main Menu", sceneManager, inputManager, resourceManager);
-    // sceneManager.registerScene<BlankScene>("Gameplay", inputManager, resourceManager);
+    resourceManager.loadFont(
+        "assets/fonts/League_Spartan/static/LeagueSpartan-Medium.ttf",
+        "LeagueSpartan");
+
+    MainMenuMock mainMenu(window, "Main Menu", sceneManager, inputManager,
+                          resourceManager);
+    // sceneManager.registerScene<BlankScene>("Gameplay", inputManager,
+    // resourceManager);
     mainMenu.simulateTestButtonClick();
     EXPECT_EQ(sceneManager.getCurrentScene(), nullptr);
 }
