fe096c1352f5b5a15a30aa70cec43fceaa56d442
Bix2llets <nhtphat2418@apcs.fitus.edu.vn>
2025-07-17 20:19:29 +0700

    [feat]: Add FPS display

--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -36,23 +36,31 @@ Application::Application() : isRunning{true} {
     // * Loading the necessary sounds
     for (auto [id, soundFile] : JSONLoader::getInstance().getAllSounds())
         ResourceManager::getInstance().loadSound(soundFile);
+
     for (auto [id, textureFile] : JSONLoader::getInstance().getAllTextures())
         ResourceManager::getInstance().loadTexture(textureFile);
+
     for (auto [id, musicFile] : JSONLoader::getInstance().getAllMusics())
         ResourceManager::getInstance().loadMusic(musicFile);
+
     for (auto [id, fontFile] : JSONLoader::getInstance().getAllFonts())
         ResourceManager::getInstance().loadFont(fontFile);
+
     for (auto [id, levelFile] : JSONLoader::getInstance().getAllLevels())
         levelFactory.loadConfig(levelFile);
+
     Logger::success("Resource loading");
     SceneManager::getInstance().registerScene<MainMenu>("Main menu");
     SceneManager::getInstance().registerScene<Setting>("Setting");
     SceneManager::getInstance().registerScene<TowerRotationMockScene>(
         "Tower Test");
+
     SceneManager::getInstance().changeScene(
         "Tower Test");  // Start with the tower test scene
+
     SceneManager::getInstance().loadLevel(
         "Gameplay", levelFactory.getLevel("exampleLevel"));
+
     SceneManager::getInstance().changeScene("Main menu");
     // sceneManager.changeScene("Setting");
 }
@@ -64,8 +72,17 @@ Application::~Application() {
 
 void Application::run() {
     sf::Clock clock;
+    
+    float fpsTime = 0.f;
     float timeElapsed = 0.f;
+    int frameCount = 0;
+    sf::Text fpsDisplay(*ResourceManager::getInstance().getFont("LeagueSpartan"));
+    fpsDisplay.setOrigin({0.f, 0.f});
+    fpsDisplay.setPosition({0.f, 0.f});
+    fpsDisplay.setFillColor(sf::Color::White);
+    fpsDisplay.setOutlineColor(sf::Color::Black);
     while (isRunning) {
+        frameCount++;
         while (auto event = Window::getInstance().pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 Window::getInstance().close();
@@ -87,13 +104,21 @@ void Application::run() {
             // SceneManager::getInstance().handleEvent(event);
         }
         // SceneManager::getInstance().handleInput();
-        timeElapsed += clock.restart().asSeconds();
+        timeElapsed += clock.getElapsedTime().asSeconds();
+        fpsTime += clock.getElapsedTime().asSeconds();
+        clock.restart();
         while (timeElapsed > GameConstants::TICK_INTERVAL) {
             SceneManager::getInstance().update();
             timeElapsed -= GameConstants::TICK_INTERVAL;
         }
+        if (fpsTime > 1.f) {
+            fpsTime -= 1.f;
+            fpsDisplay.setString(std::to_string(frameCount));
+            frameCount = 0;
+        }
         Window::getInstance().clear(sf::Color::Black);
         SceneManager::getInstance().render();
+        Window::getInstance().draw(fpsDisplay);
         Window::getInstance().draw(Cursor::getInstance());
         Window::getInstance().display();
     }
