3265df0b2101c8389c43e7550aa590ab21ea76dc
Bix2llets <nhtphat2418@apcs.fitus.edu.vn>
2025-07-27 16:43:38 +0700

    [fix]: Soft clamping the begin and end point for visual effects

--- src/Gameplay/PathGenerator.cpp
+++ src/Gameplay/PathGenerator.cpp
@@ -9,19 +9,20 @@ PathGenerator::PathGenerator(
     const std::vector<std::vector<Height::Height>> &heightMap)
     : heightMap{heightMap} {
     transitionCost.resize(8);
-    transitionCost[Height::DeepSea] =      {000, 400, 300, 200, 300, 400, 450, 500};
-    transitionCost[Height::ShallowWater] = {900, 000, 300, 100, 200, 400, 500, 900};
-    transitionCost[Height::Sand] =         {900, 550, 000, 300, 450, 500, 550, 900};
-    transitionCost[Height::Plain] =        {900, 750, 600, 000, 600, 700, 800, 900};
-    transitionCost[Height::Forest] =       {900, 700, 500, 300, 000, 600, 750, 900};
-    transitionCost[Height::Rocky] =        {900, 500, 400, 200, 300, 000, 600, 900};
-    transitionCost[Height::Mountain] =     {900, 500, 250, 100, 250, 350, 000, 700};
-    transitionCost[Height::SnowCap] =      {900, 160, 120, 100, 120, 160, 200, 000};
+    transitionCost[Height::DeepSea] = {000, 400, 300, 200, 300, 400, 450, 500};
+    transitionCost[Height::ShallowWater] = {900, 000, 300, 100,
+                                            200, 400, 500, 900};
+    transitionCost[Height::Sand] = {900, 550, 000, 300, 450, 500, 550, 900};
+    transitionCost[Height::Plain] = {900, 750, 600, 000, 600, 700, 800, 900};
+    transitionCost[Height::Forest] = {900, 700, 500, 300, 000, 600, 750, 900};
+    transitionCost[Height::Rocky] = {900, 500, 400, 200, 300, 000, 600, 900};
+    transitionCost[Height::Mountain] = {900, 500, 250, 100, 250, 350, 000, 700};
+    transitionCost[Height::SnowCap] = {900, 160, 120, 100, 120, 160, 200, 000};
 }
 
 std::vector<sf::Vector2f> PathGenerator::operator()(sf::Vector2i begin,
                                                     sf::Vector2i end) {
-    std::ofstream logFile("log.txt");
+    // std::ofstream logFile("log.txt");
     static const int INF = INT_MAX;
     std::vector<std::vector<int>> f(
         heightMap.size(), std::vector<int>(heightMap[0].size(), INT_MAX));
@@ -35,16 +36,16 @@ std::vector<sf::Vector2f> PathGenerator::operator()(sf::Vector2i begin,
         return std::vector<sf::Vector2f>();
     if (heightMap[0].size() < std::max(begin.y, end.y))
         return std::vector<sf::Vector2f>();
-
+    static const int SOFT_BOUND = 5;
     std::priority_queue<Weight, std::vector<Weight>, WeightCmp> weight;
     static sf::Vector2i direction[4] = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
     auto isInBound = [this, begin, end](sf::Vector2i position) {
-        // if (position.x < 0) return false;
-        // if (position.y < 0) return false;
-        if (position.x >= heightMap[0].size()) return false;
-        if (position.y >= heightMap.size()) return false;
-        if (position.x > end.x) return false;
-        if (position.y > end.y) return false;
+        if (position.x < 0 + SOFT_BOUND) return false;
+        if (position.y < 0 + SOFT_BOUND) return false;
+        if (position.x >= heightMap[0].size() - SOFT_BOUND) return false;
+        if (position.y >= heightMap.size() - SOFT_BOUND) return false;
+        // if (position.x > end.x) return false;
+        // if (position.y > end.y) return false;
 
         return true;
     };
@@ -53,6 +54,17 @@ std::vector<sf::Vector2f> PathGenerator::operator()(sf::Vector2i begin,
         const int LENGTH = 10;
         return LENGTH * (abs(position.x - end.x) + abs(position.y - end.y));
     };
+
+    auto clampBound = [this](sf::Vector2i &value) {
+        value.x =
+            std::max((int)SOFT_BOUND,
+                     std::min(value.x, (int)heightMap[0].size() - SOFT_BOUND));
+        value.y =
+            std::max((int)SOFT_BOUND,
+                     std::min(value.y, (int)heightMap.size() - SOFT_BOUND));
+    };
+    clampBound(begin);
+    clampBound(end);
     weight.push({begin.x, begin.y, 0, getHeuristic(begin)});
     f[begin.y][begin.x] = 0;
     g[begin.y][begin.x] = 0;
@@ -66,7 +78,9 @@ std::vector<sf::Vector2f> PathGenerator::operator()(sf::Vector2i begin,
         if (current.x == end.x && current.y == end.y) {
             break;
         }
-        logFile << std::format("Choosing {} {} with f = {}", current.x, current.y, current.distanceFromStart + current.distanceToEnd) << "\n";
+        // logFile << std::format("Choosing {} {} with f = {}", current.x,
+        // current.y, current.distanceFromStart + current.distanceToEnd) <<
+        // "\n";
         for (int i = 0; i < 4; i++) {
             Weight nextCell;
             nextCell.x = current.x + direction[i].x;
@@ -79,7 +93,9 @@ std::vector<sf::Vector2f> PathGenerator::operator()(sf::Vector2i begin,
                               [heightMap[nextCell.y][nextCell.x]];
             nextCell.distanceToEnd = getHeuristic({nextCell.x, nextCell.y});
             bool canMove = false;
-            if (f[nextCell.y][nextCell.x] > nextCell.distanceFromStart + nextCell.distanceToEnd) canMove = true;
+            if (f[nextCell.y][nextCell.x] >
+                nextCell.distanceFromStart + nextCell.distanceToEnd)
+                canMove = true;
             // if (nextCell.distanceFromStart + nextCell.distanceToEnd ==
             //     f[nextCell.y][nextCell.x])
             //     if (nextCell.distanceFromStart < g[nextCell.y][nextCell.x])
@@ -91,12 +107,12 @@ std::vector<sf::Vector2f> PathGenerator::operator()(sf::Vector2i begin,
                 previousCells[nextCell.y][nextCell.x] = {current.x, current.y};
                 weight.push(nextCell);
 
-                logFile << std::format(
-                    "{} {} {} {} {} {}", nextCell.x, nextCell.y,
-                    nextCell.distanceFromStart, nextCell.distanceToEnd,
-                    transitionCost[heightMap[current.y][current.x]]
-                                  [heightMap[nextCell.y][nextCell.x]],
-                    heightCost[heightMap[nextCell.y][nextCell.x]]) << "\n";
+                // logFile << std::format(
+                //     "{} {} {} {} {} {}", nextCell.x, nextCell.y,
+                //     nextCell.distanceFromStart, nextCell.distanceToEnd,
+                //     transitionCost[heightMap[current.y][current.x]]
+                //                   [heightMap[nextCell.y][nextCell.x]],
+                //     heightCost[heightMap[nextCell.y][nextCell.x]]) << "\n";
             }
         }
     }
@@ -116,10 +132,10 @@ std::vector<sf::Vector2f> PathGenerator::operator()(sf::Vector2i begin,
         Logger::debug(std::format("{} {}", node.x, node.y));
     }
 
-    for (auto x : f) {
-        for (auto y : x) logFile << y << " ";
-        logFile << "\n";
-    }
-    logFile << "\n";
+    // for (auto x : f) {
+    //     for (auto y : x) logFile << y << " ";
+    //     logFile << "\n";
+    // }
+    // logFile << "\n";
     return result;
 }
